d75f61137e1f0733221fbc04a4423fbff56a9538
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2023-06-13 10:11:43 +0300

    app main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+
+const { PORT } = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.use(helmet());
+  await app.listen(PORT);
 }
 bootstrap();
